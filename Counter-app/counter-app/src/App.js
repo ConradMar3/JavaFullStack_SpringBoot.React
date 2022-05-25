310225bed82527e5503eac7f12cc98eae28c3c97
ConradMar3 <conradmare890317@gmail.com>
2022-05-25 06:23:58 -0700

    2022/05/25 (pm) Adding State to React Counter Component.

--- Counter-app/counter-app/src/App.js
+++ Counter-app/counter-app/src/App.js
@@ -2,19 +2,29 @@ import React, { Component } from 'react';
 import FirstComponent from './components/learning-examples/FirstComponent';
 import SecondComponent from './components/learning-examples/SecondComponent';
 import ThirdComponent from './components/learning-examples/ThirdComponent';
-//import logo from './logo.svg';
+import Counter from './components/counter/Counter';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Counter></Counter>
+      </div>
+    );
+  }
+}
+
+class LearningComponents extends Component {
+  render() {
+    return (
+      <div className="LearningComponents">
         <h1><strong><i>Hello World!</i></strong></h1>
         <FirstComponent></FirstComponent>
         <SecondComponent></SecondComponent>
         <ThirdComponent></ThirdComponent>
       </div>
-    );
+    )
   }
 }
 
