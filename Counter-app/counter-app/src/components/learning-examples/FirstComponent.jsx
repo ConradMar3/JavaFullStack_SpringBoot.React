fa02ab35cdbd5f42ec8de445ac5a98491355154e
ConradMar3 <conradmare890317@gmail.com>
2022-05-25 05:22:30 -0700

    2022/05/25 (pm) New App - 'Counter.app'

---
+++ Counter-app/counter-app/src/components/learning-examples/FirstComponent.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class FirstComponent extends Component {
+    render() {
+        return (
+            <div className="firstComponent">
+                <h2>FirstComponent</h2>
+            </div>
+        );
+    }
+}
+
+export default FirstComponent;
---
+++ Counter-app/counter-app/src/components/learning-examples/SecondComponent.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SecondComponent extends Component {
+    render() {
+        return (
+            <div className="secondComponent">
+                <h3>SecondComponent</h3>
+            </div>
+        );
+    }
+}
+
+export default SecondComponent;
